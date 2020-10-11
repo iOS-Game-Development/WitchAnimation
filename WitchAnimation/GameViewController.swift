26f5ae566aeee480fa3edc4a5802de83f6588157
Hung Phan <hungphan@Hs-Mac-mini.local>
2020-10-10 23:15:16 -0700

    fix: full screen requires a LaunchScreen.storyboard and set Launch Screen File in General

--- WitchAnimation/GameViewController.swift
+++ WitchAnimation/GameViewController.swift
@@ -14,21 +14,39 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
             
-        if let view = self.view as? SKView {
-            let scene = GameScene(size: view.frame.size)
-            print("width: \(UIScreen.main.bounds.width)")
-            print("height: \(UIScreen.main.bounds.height)")
+//        if let view = self.view as? SKView {
+//            let scene = GameScene(size: view.frame.size)
+//            print("width: \(UIScreen.main.bounds.width)")
+//            print("height: \(UIScreen.main.bounds.height)")
+//
+//
+//            scene.scaleMode = .aspectFill
+//            scene.size = view.bounds.size
+//
+//            view.ignoresSiblingOrder = true
+//
+//            view.showsFPS = true
+//            view.showsNodeCount = true
+//            // Present the scene
+//            view.presentScene(scene)
+//        }
+        guard let view = self.view as? SKView else { return }
+        let scene = GameScene(size: view.frame.size)
+        
+        view.ignoresSiblingOrder = true
+        view.presentScene(scene)
             
-            
-            scene.scaleMode = .aspectFill
-            
-            view.ignoresSiblingOrder = true
-                
-            view.showsFPS = true
-            view.showsNodeCount = true
-            // Present the scene
-            view.presentScene(scene)
-        }
-            
+    }
+    
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .landscape
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
     }
 }
